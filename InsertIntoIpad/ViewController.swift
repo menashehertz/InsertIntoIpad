61b744ef49e22e2a5c01b5988750be70d3fe7c9d
Steven Hertz <hertzsteven@icloud.com>
2020-03-03 18:55:25 -0500

    Initial Commit

---
+++ InsertIntoIpad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InsertIntoIpad
+//
+//  Created by Steven Hertz on 3/3/20.
+//  Copyright © 2020 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
